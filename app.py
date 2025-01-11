c88ea5465b59e6299c40909cbae4472b9b59a68f
Rahil Shaikh <rahilshaikh7117@gmail.com>
2025-01-11 22:05:10 +0530

    Main changes

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 from flask import Flask, render_template, request
 from flask_mail import Mail, Message
 import os
-from time import time
 from dotenv import load_dotenv
 
 load_dotenv()
 
 app = Flask(__name__)
 
+sender_mailid=os.getenv('mail_id')
+
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'rahilss2003@gmail.com'
+app.config['MAIL_USERNAME'] = sender_mailid
 app.config['MAIL_PASSWORD'] = os.getenv('gmail_password')
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
@@ -23,10 +24,10 @@ def home():
         user_subject = request.form.get('subject')
         user_body = request.form.get('body')
         # print(user_email, " ", user_subject, " ", user_body)
-        msg = Message(user_subject, sender='rahilss2003@gmail.com', recipients=[user_email])
+        msg = Message(user_subject, sender=sender_mailid, recipients=[user_email])
         msg.body = user_body
         mail.send(msg)
-        return render_template('index.html',current_time=time)
+        return render_template('index.html')
     else:
         return render_template('index.html')
 
